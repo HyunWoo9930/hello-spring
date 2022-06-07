c044d617e40d53fe846f508879346cca7a43b4ec
dhgusdn <hw62459930@gmail.com>
2022-06-07 12:48:41 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,17 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 id를 1씩 올려주는 방식
   private long id;
+
   private String name;
 
   public long getId() {
